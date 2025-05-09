576480765fe48305d9cc42d380beb704b2f1f246
daswi <daswi@DANIEL>
2025-05-08 22:45:57 -0400

    delete de solicitudes

--- src/routes/Solicitudes.routes.js
+++ src/routes/Solicitudes.routes.js
@@ -13,6 +13,6 @@ router.get('/solicitudes', getSolicitudes);
 router.get('/solicitudes/:id', getSolicitud);
 router.post('/solicitudes', createSolicitud);
 router.put('/solicitudes/:id', updateSolicitud);
-router.delete('/solicitudes-borrar/:id', deleteSolicitud);
+router.delete('/solicitudes/:id', deleteSolicitud);
 
 export default router;
