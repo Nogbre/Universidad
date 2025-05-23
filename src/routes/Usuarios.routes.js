88db56513f878dd4202de7347268f927377d6893
Lucas <Lucasaguilarn@gmail.com>
2025-05-23 08:31:10 -0400

    Conseguir todos los usuarios

--- src/routes/Usuarios.routes.js
+++ src/routes/Usuarios.routes.js
@@ -3,12 +3,13 @@ import {
     getAllUsers,
     getUsersByType,
     getUserById
-} from './controllers.js';
+} from '../controllers/Usuarios.controllers.js';
 
 const router = express.Router();
 
-router.get('/', getAllUsers);
-router.get('/:tipo', getUsersByType);
-router.get('/:tipo/:id', getUserById);
+router.get('/users', getAllUsers);
+router.get('/users/type/:tipo', getUsersByType);
+router.get('/users/type/:tipo/id/:id', getUserById);
+
 
 export default router;
